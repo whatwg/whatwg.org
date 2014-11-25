8e92ccf42277cfa934a5f062e1831905be35a393
Domenic Denicola <d@domenic.me>
2014-11-25 18:58:54 -0500

    Add file-issue.js
    Closes #4.

---
+++ file-issue.js
@@ -0,0 +1,118 @@
+// Usage: include a link like `<a href="https://github.com/whatwg/streams/issues/new">file an issue</a>`, or give it
+// `id="file-issue-link"` instead. Then include this script with
+// `<script src="https://resources.whatwg.or/file-issue.js" async></script>`. Style the element using the selector
+// `.selected-text-file-an-issue`.
+
+(function () {
+  "use strict";
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var link = findLink();
+    var originalHref = link.href;
+    var prevSelectionText = null;
+
+    var fileLink = document.createElement('a');
+    fileLink.href = getFilingUrl(originalHref, window.getSelection());
+    fileLink.accessKey = '1';
+    fileLink.className = 'selected-text-file-an-issue';
+    fileLink.textContent = 'File an issue about the selected text';
+
+    document.body.appendChild(fileLink);
+
+    window.addEventListener('mouseup', handleInteraction);
+    window.addEventListener('keydown', handleInteraction);
+
+    function handleInteraction(event) {
+      var selection = window.getSelection();
+      if (selection.toString() === prevSelectionText) {
+        return;
+      }
+
+      fileLink.href = getFilingUrl(originalHref, selection, event.target);
+    }
+  });
+
+  function findLink() {
+    var link = document.querySelector('#file-issue-link, a[href$="/issues/new"]');
+
+    if (link) {
+      return link;
+    }
+
+    throw new Error('No "file an issue" link found');
+  }
+
+  function getFilingUrl(originalHref, selection, startNode) {
+    var bugData = getBugData(selection, startNode);
+    return originalHref + '?title=' + encodeURIComponent(bugData.title) + '&body=' + encodeURIComponent(bugData.body);
+  }
+
+  function getBugData(selection, startNode) {
+    var selectionText = selection.toString();
+    var url = getUrlToReport(selection, startNode);
+
+    return {
+      title: getTitle(selectionText),
+      body: getBody(url, selectionText)
+    };
+  }
+
+  function getBody(url, selectionText) {
+    var quotedText = selectionText;
+    if (quotedText.length > 1000) {
+      quotedText = quotedText.substring(0, 997) + '...';
+    }
+
+    quotedText = quotedText.replace(/\r/g, '').replace(/\n\n/g, '\n\n> ');
+    if (quotedText.length > 0) {
+      quotedText = '> ' + quotedText;
+    }
+
+    return url + '\n\n' + quotedText;
+  }
+
+  function getTitle(selectionText) {
+    var title = selectionText.toString().replace(/\n/g, ' ');
+    if (title.length > 50) {
+      title = title.substring(0, 47) + '...';
+    }
+
+    if (title.length > 0) {
+      title = '"' + title + '"';
+    }
+
+    return title;
+  }
+
+  function getUrlToReport(selection, startNode) {
+    var url = window.location.href;
+
+    var node = getBestNodeToReport(selection, startNode);
+    if (node) {
+      url += '#' + node.id;
+    }
+
+    return url;
+  }
+
+  function getBestNodeToReport(selection, fallback) {
+    var node = fallback;
+
+    if (selection.anchorNode) {
+      node = selection.anchorNode;
+
+      if (selection.focusNode && selection.focusNode.compareDocumentPosition) {
+        var compare = selection.focusNode.compareDocumentPosition(selection.anchorNode);
+        if (compare & Node.DOCUMENT_POSITION_FOLLOWING || compare & Node.DOCUMENT_POSITION_CONTAINED_BY) {
+          node = selection.focusNode;
+        }
+      }
+    }
+
+    while (node && !node.id) {
+      node = node.previousSibling || node.parentNode;
+    }
+
+    return node;
+  }
+}());
