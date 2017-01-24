56109a1a0575b81d7c12bc5273677249856e31ef
Anne van Kesteren <annevk@annevk.nl>
2017-01-24 19:57:10 +0100

    Enable deploy from Travis
    This moves the whole website into /src for easy of copying. And adds a
    script to deploy from Travis when a commit lands in master.

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ "$TRAVIS_BRANCH" != "master" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Skipping deploy for a pull request"
+else
+    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
+    echo "$SERVER $SERVER_PUBLIC_KEY" > known_hosts
+    rsync --archive --verbose --compress --rsh="ssh -o UserKnownHostsFile=known_hosts" src/ $DEPLOY_USER@$SERVER:$WEB_ROOT/
+fi
