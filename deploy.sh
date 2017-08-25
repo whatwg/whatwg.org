c7fa535bea549add1d130db63d9dbabe16467637
Philip Jägenstedt <philip@foolip.org>
2017-08-25 13:37:26 +0200

    Extract out spec.whatwg.org to its own top-level dir
    A copy is made to keep this working on both the old and new server.

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,8 @@ else
     ssh-add deploy_key
     echo "$SERVER $SERVER_PUBLIC_KEY" > known_hosts
     echo "$NEW_SERVER $NEW_SERVER_PUBLIC_KEY" >> known_hosts
+    # TODO: remove this copy when the old server is gone
+    cp spec.whatwg.org/index.html whatwg.org/specs/index
     rsync --archive --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" whatwg.org/ $DEPLOY_USER@$SERVER:whatwg.org/
-    rsync --archive --chmod=D755,F644 --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" idea.whatwg.org images.whatwg.org n.whatwg.org whatwg.org deploy@$NEW_SERVER:/var/www/
+    rsync --archive --chmod=D755,F644 --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" whatwg.org *.whatwg.org deploy@$NEW_SERVER:/var/www/
 fi
