1a0aa536faad4eeeffaf590e46c4a5bcc0d1077b
Anne van Kesteren <annevk@annevk.nl>
2017-08-31 13:41:19 +0200

    so close

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,12 @@ else
     ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
     openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in deploy_key.enc -out deploy_key -d
     chmod 600 deploy_key
-    eval $(ssh-agent -s)
+    eval "$(ssh-agent -s)"
     ssh-add deploy_key
     echo "$SERVER $SERVER_PUBLIC_KEY" > known_hosts
     echo "$NEW_SERVER $NEW_SERVER_PUBLIC_KEY" >> known_hosts
     # TODO: remove this copy when the old server is gone
     cp spec.whatwg.org/index.html whatwg.org/specs/index
-    rsync --archive --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" whatwg.org resources.whatwg.org "$DEPLOY_USER@$SERVER:"
+    rsync --archive --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" ./whatwg.org ./resources.whatwg.org "$DEPLOY_USER@$SERVER:"
     rsync --archive --chmod="D755,F644" --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" ./whatwg.org ./*.whatwg.org "deploy@$NEW_SERVER:/var/www/"
 fi
