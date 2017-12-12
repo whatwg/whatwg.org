058765b307a702ab14c23844c9311d0fa6b28394
Maciej Stachowiak <maciej.s@me.com>
2017-12-12 14:32:40 -0800

    Put everything at top level.

--- convert-all-policies.sh
+++ convert-all-policies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./whatwg.org/convert-policy.py ../sg/SG\ Policy.md whatwg.org/policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/sg-policy
-./whatwg.org/convert-policy.py ../sg/Workstream\ Policy.md whatwg.org/policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/workstream-policy
-./whatwg.org/convert-policy.py ../sg/SG\ Agreement.md whatwg.org/policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/sg-agreement
-./whatwg.org/convert-policy.py ../sg/Principles.md whatwg.org/policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/principles
-./whatwg.org/convert-policy.py ../sg/IPR\ Policy.md whatwg.org/policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/ipr-policy
+./convert-policy.py ../sg/SG\ Policy.md policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/sg-policy
+./convert-policy.py ../sg/Workstream\ Policy.md policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/workstream-policy
+./convert-policy.py ../sg/SG\ Agreement.md policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/sg-agreement
+./convert-policy.py ../sg/Principles.md policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/principles
+./convert-policy.py ../sg/IPR\ Policy.md policy-template.html ../sg/policy-link-mapping.txt  > whatwg.org/ipr-policy
 
