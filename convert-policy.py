whatwg/whatwg.org

commit 4d34512aeb618c8753e317b220a7937c2161fb11

Redesign the rest of the site
Closes #21. Closes #59. Closes #178. Closes #180. Helps with #149.

convert-policy.py
@@ -86,14 +86,13 @@ def preprocess_markdown(policy_markdown, link_mapping):
 
 def markdown_title(policy_markdown):
     for line in policy_markdown.split('\n'):
-        if line.startswith("#"):
-            title = line.lstrip("# ")
-            replacement_line = re.sub(r'^# WHATWG ', '# ', line)
-            policy_markdown = policy_markdown.replace(line, replacement_line)
+        if line.startswith("# WHATWG "):
+            title = line.lstrip("# WHATWG ")
+            policy_markdown = policy_markdown.replace(line, "")
 
             return (title, policy_markdown)
 
-    return ("WHATWG Policies", policy_markdown)
+    return ("Policies", policy_markdown)
 
 
 def main():

idea.whatwg.org/index.html
@@ -1,39 +1,43 @@
 <!DOCTYPE HTML>
 <html lang="en">
- <head>
-  <title>Web Hypertext Application Technology Working Group Ideas</title>
-  <link rel="stylesheet" href="https://whatwg.org/style/tabbed-pages">
-  <link rel="icon" href="https://resources.whatwg.org/logo.svg">
- </head>
- <body>
+<meta charset="utf-8">
+<title>Ideas — WHATWG</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#3A7908">
+<link rel="icon" href="https://resources.whatwg.org/logo.svg">
+<link rel="stylesheet" href="https://whatwg.org/style/shared.css">
+<link rel="stylesheet" href="https://whatwg.org/style/subpages.css">
+
+<header>
+ <hgroup>
   <h1>
-   <span class="the">The</span>
-   <strong class="what">Web Hypertext Application Technology</strong>
-   <span class="wg">Working Group</span>
+   <a href="/">
+    <img id="main-logo" src="https://resources.whatwg.org/logo.svg" alt="">
+    WHATWG
+   </a>
   </h1>
-  <ul class="navigation">
-   <li><a href="https://whatwg.org/" rel="home">Home</a></li>
-   <li><a href="https://spec.whatwg.org/">Standards</a></li>
-   <li><a href="https://whatwg.org/faq">FAQ</a></li>
-   <li><a href="https://whatwg.org/policies">Policies</a></li>
-   <li><a href="https://participate.whatwg.org/">Participate</a></li>
-  </ul>
-
   <h2>Ideas</h2>
+ </hgroup>
+</header>
 
-  <p>The WHATWG works on a number of ideas that aspire to become web platform standards.</p>
+<nav class="buttonish-links">
+ <a href="https://spec.whatwg.org/">Standards</a>
+ <a href="https://whatwg.org/faq">FAQ</a>
+ <a href="https://whatwg.org/policies">Policies</a>
+ <a href="https://participate.whatwg.org/">Participate</a>
+</nav>
 
-  <dl>
-   <dt><a href="https://books.idea.whatwg.org/">CSS Books</a></dt>
-   <dd><p>CSS Books defines CSS features for book publishing and high-quality printing of web
-   pages.</p></dd>
+<p>The WHATWG works on a number of ideas that aspire to become web platform standards.</p>
 
-   <dt><a href="https://figures.idea.whatwg.org/">CSS Figures</a></dt>
-   <dd><p>CSS Figures defines CSS features for floating elements around pages and columns.</p></dd>
-  </dl>
+<dl>
+ <dt><a href="https://books.idea.whatwg.org/">CSS Books</a></dt>
+ <dd><p>CSS Books defines CSS features for book publishing and high-quality printing of web
+ pages.</p></dd>
 
-  <footer>
-   <p><small>Copyright © 2017 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
-  </footer>
- </body>
-</html>
+ <dt><a href="https://figures.idea.whatwg.org/">CSS Figures</a></dt>
+ <dd><p>CSS Figures defines CSS features for floating elements around pages and columns.</p></dd>
+</dl>
+
+<footer>
+ <p><small>Copyright © 2018 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
+</footer>

policy-template.html
@@ -1,25 +1,30 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="en">
- <head>
-  <title>@TITLE_GOES_HERE@</title>
-  <link rel="stylesheet" href="/style/tabbed-pages">
-  <link rel="icon" href="https://resources.whatwg.org/logo.svg">
- </head>
- <body>
+<meta charset="utf-8">
+<title>@TITLE_GOES_HERE@ — WHATWG</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#3A7908">
+<link rel="icon" href="https://resources.whatwg.org/logo.svg">
+<link rel="stylesheet" href="/style/shared.css">
+<link rel="stylesheet" href="/style/subpages.css">
+
+<header>
+ <hgroup>
   <h1>
-   <span class="the">The</span>
-   <strong class="what">Web Hypertext Application Technology</strong>
-   <span class="wg">Working Group</span>
+   <a href="/">
+    <img id="main-logo" src="https://resources.whatwg.org/logo.svg" alt="">
+    WHATWG
+   </a>
   </h1>
-  <ul class="navigation">
-   <li><a href="/" rel="home">Home</a></li>
-   <li><a href="https://spec.whatwg.org/">Standards</a></li>
-   <li><a href="/faq">FAQ</a></li>
-   <li class="this"><a href="/policies">Policies</a></li>
-   <li><a href="https://participate.whatwg.org/">Participate</a></li>
-  </ul>
+  <h2>@TITLE_GOES_HERE@</h2>
+ </hgroup>
+</header>
+
+<nav class="buttonish-links">
+ <a href="https://spec.whatwg.org/">Standards</a>
+ <a href="/faq">FAQ</a>
+ <a>Policies</a>
+ <a href="https://participate.whatwg.org/">Participate</a>
+</nav>
 
 @POLICY_GOES_HERE@
-
- </body>
-</html>

spec.whatwg.org/index.html
@@ -1,122 +1,125 @@
 <!DOCTYPE HTML>
-<!-- Note: this resource is served from https://spec.whatwg.org/ -->
 <html lang="en">
- <head>
-  <title>Web Hypertext Application Technology Working Group Standards</title>
-  <link rel="stylesheet" href="https://whatwg.org/style/tabbed-pages">
-  <link rel="icon" href="https://resources.whatwg.org/logo.svg">
- </head>
- <body>
+<meta charset="utf-8">
+<title>Standards — WHATWG</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#3A7908">
+<link rel="icon" href="https://resources.whatwg.org/logo.svg">
+<link rel="stylesheet" href="https://whatwg.org/style/shared.css">
+<link rel="stylesheet" href="https://whatwg.org/style/subpages.css">
+
+<header>
+ <hgroup>
   <h1>
-   <span class="the">The</span>
-   <strong class="what">Web Hypertext Application Technology</strong>
-   <span class="wg">Working Group</span>
+   <a href="/">
+    <img id="main-logo" src="https://resources.whatwg.org/logo.svg" alt="">
+    WHATWG
+   </a>
   </h1>
-  <ul class="navigation">
-   <li><a href="https://whatwg.org/" rel="home">Home</a></li>
-   <li class="this"><strong>Standards</strong></li>
-   <li><a href="https://whatwg.org/faq">FAQ</a></li>
-   <li><a href="https://whatwg.org/policies">Policies</a></li>
-   <li><a href="https://participate.whatwg.org/">Participate</a></li>
-  </ul>
-
   <h2>Standards</h2>
+ </hgroup>
+</header>
 
-  <p>The WHATWG works on a number of technologies that are fundamental parts of the web platform.
-  They are organised somewhat arbitrarily based on the preferences of those editing the standard for
-  those technologies.</p>
+<nav class="buttonish-links">
+ <a>Standards</a>
+ <a href="https://whatwg.org/faq">FAQ</a>
+ <a href="https://whatwg.org/policies">Policies</a>
+ <a href="https://participate.whatwg.org/">Participate</a>
+</nav>
 
-  <dl>
-   <dt><a href="https://compat.spec.whatwg.org/">Compatibility</a></dt>
-   <dd>
-    <p>The Compatibility Standard describes a collection of non-standard (and often vendor-prefixed)
-    CSS properties and DOM APIs that web browsers need to support for compatibility with the de
-    facto web.</p>
-   </dd>
+<p>The WHATWG works on a number of technologies that are fundamental parts of the web platform.
+They are organised somewhat arbitrarily based on the preferences of those editing the standard for
+those technologies.</p>
 
-   <dt><a href="https://console.spec.whatwg.org/">Console</a></dt>
-   <dd>
-    <p>The Console Standard defines APIs for console debugging facilities.</p>
-   </dd>
+<dl>
+ <dt><a href="https://compat.spec.whatwg.org/">Compatibility</a></dt>
+ <dd>
+  <p>The Compatibility Standard describes a collection of non-standard (and often vendor-prefixed)
+  CSS properties and DOM APIs that web browsers need to support for compatibility with the de
+  facto web.</p>
+ </dd>
 
-   <dt><a href="https://dom.spec.whatwg.org/">DOM</a></dt>
-   <dd>
-    <p>The DOM Standard defines the core infrastructure used to define the web.</p>
-   </dd>
+ <dt><a href="https://console.spec.whatwg.org/">Console</a></dt>
+ <dd>
+  <p>The Console Standard defines APIs for console debugging facilities.</p>
+ </dd>
 
-   <dt><a href="https://encoding.spec.whatwg.org/">Encoding</a></dt>
-   <dd>
-    <p>The Encoding Standard defines how character encodings work on the web.</p>
-   </dd>
+ <dt><a href="https://dom.spec.whatwg.org/">DOM</a></dt>
+ <dd>
+  <p>The DOM Standard defines the core infrastructure used to define the web.</p>
+ </dd>
 
-   <dt><a href="https://fetch.spec.whatwg.org/">Fetch</a></dt>
-   <dd>
-    <p>The Fetch Standard defines the networking model for resource retrieval on the web.</p>
-   </dd>
+ <dt><a href="https://encoding.spec.whatwg.org/">Encoding</a></dt>
+ <dd>
+  <p>The Encoding Standard defines how character encodings work on the web.</p>
+ </dd>
 
-   <dt><a href="https://fullscreen.spec.whatwg.org/">Fullscreen API</a></dt>
-   <dd>
-    <p>The Fullscreen API Standard defines how web pages can take over a user's entire screen (at
-    the user's request), e.g. for gaming or to watch a video.</p>
-   </dd>
+ <dt><a href="https://fetch.spec.whatwg.org/">Fetch</a></dt>
+ <dd>
+  <p>The Fetch Standard defines the networking model for resource retrieval on the web.</p>
+ </dd>
 
-   <dt><a href="https://html.spec.whatwg.org/multipage/">HTML</a></dt>
-   <dd>
-    <p>The HTML Standard is a kitchen sink full of technologies for the web. It includes the core
-    markup language for the web, HTML, as well as numerous APIs like Web Sockets, Web Workers,
-    <code>localStorage</code>, etc.</p>
-   </dd>
+ <dt><a href="https://fullscreen.spec.whatwg.org/">Fullscreen API</a></dt>
+ <dd>
+  <p>The Fullscreen API Standard defines how web pages can take over a user's entire screen (at
+  the user's request), e.g. for gaming or to watch a video.</p>
+ </dd>
 
-   <dt><a href="https://infra.spec.whatwg.org/">Infra</a></dt>
-   <dd>
-    <p>The Infra Standard aims to define the fundamental concepts upon which standards are built.</p>
-   </dd>
+ <dt><a href="https://html.spec.whatwg.org/multipage/">HTML</a></dt>
+ <dd>
+  <p>The HTML Standard is a kitchen sink full of technologies for the web. It includes the core
+  markup language for the web, HTML, as well as numerous APIs like Web Sockets, Web Workers,
+  <code>localStorage</code>, etc.</p>
+ </dd>
 
-   <dt><a href="https://mimesniff.spec.whatwg.org/">MIME Sniffing</a></dt>
-   <dd>
-    <p>The MIME Sniffing Standard defines algorithms used to determine the type of resources.</p>
-   </dd>
+ <dt><a href="https://infra.spec.whatwg.org/">Infra</a></dt>
+ <dd>
+  <p>The Infra Standard aims to define the fundamental concepts upon which standards are built.</p>
+ </dd>
 
-   <dt><a href="https://notifications.spec.whatwg.org/">Notifications API</a></dt>
-   <dd>
-    <p>The Notifications API Standard provides an API to display notifications to alert users
-    outside the context of a web page.</p>
-   </dd>
+ <dt><a href="https://mimesniff.spec.whatwg.org/">MIME Sniffing</a></dt>
+ <dd>
+  <p>The MIME Sniffing Standard defines algorithms used to determine the type of resources.</p>
+ </dd>
 
-   <dt><a href="https://quirks.spec.whatwg.org/">Quirks Mode</a></dt>
-   <dd>
-    <p>The Quirks Mode Standard describes behaviours in CSS and Selectors that are not yet defined
-    in the relevant specifications but that are nonetheless widely implemented.</p>
-   </dd>
+ <dt><a href="https://notifications.spec.whatwg.org/">Notifications API</a></dt>
+ <dd>
+  <p>The Notifications API Standard provides an API to display notifications to alert users
+  outside the context of a web page.</p>
+ </dd>
 
-   <dt><a href="https://storage.spec.whatwg.org/">Storage</a></dt>
-   <dd>
-    <p>The Storage Standard defines an API for persistent storage and quota estimates, as well as
-    the platform storage architecture.</p>
-   </dd>
+ <dt><a href="https://quirks.spec.whatwg.org/">Quirks Mode</a></dt>
+ <dd>
+  <p>The Quirks Mode Standard describes behaviours in CSS and Selectors that are not yet defined
+  in the relevant specifications but that are nonetheless widely implemented.</p>
+ </dd>
 
-   <dt><a href="https://streams.spec.whatwg.org/">Streams</a></dt>
-   <dd>
-    <p>The Streams Standard provides APIs for creating, composing, and consuming streams of data
-    that map efficiently to low-level I/O primitives.</p>
-   </dd>
+ <dt><a href="https://storage.spec.whatwg.org/">Storage</a></dt>
+ <dd>
+  <p>The Storage Standard defines an API for persistent storage and quota estimates, as well as
+  the platform storage architecture.</p>
+ </dd>
 
-   <dt><a href="https://url.spec.whatwg.org/">URL</a></dt>
-   <dd>
-    <p>The URL Standard defines the infrastructure around URLs on the web.</p>
-   </dd>
+ <dt><a href="https://streams.spec.whatwg.org/">Streams</a></dt>
+ <dd>
+  <p>The Streams Standard provides APIs for creating, composing, and consuming streams of data
+  that map efficiently to low-level I/O primitives.</p>
+ </dd>
 
-   <dt><a href="https://xhr.spec.whatwg.org/">XMLHttpRequest</a></dt>
-   <dd>
-    <p>The XMLHttpRequest Standard defines the networking API exposed to scripts on the web.</p>
-   </dd>
-  </dl>
+ <dt><a href="https://url.spec.whatwg.org/">URL</a></dt>
+ <dd>
+  <p>The URL Standard defines the infrastructure around URLs on the web.</p>
+ </dd>
 
-  <p>The WHATWG also works on a <a href="https://idea.whatwg.org/">number of ideas</a> that aspire
-  to become standards one day.</p>
+ <dt><a href="https://xhr.spec.whatwg.org/">XMLHttpRequest</a></dt>
+ <dd>
+  <p>The XMLHttpRequest Standard defines the networking API exposed to scripts on the web.</p>
+ </dd>
+</dl>
 
-  <footer>
-   <p><small>Copyright © 2017 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
-  </footer>
- </body>
-</html>
+<p>The WHATWG also works on a <a href="https://idea.whatwg.org/">number of ideas</a> that aspire
+to become standards one day.</p>
+
+<footer>
+ <p><small>Copyright © 2018 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
+</footer>

whatwg.org/404
@@ -1,35 +1,39 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="en">
- <head>
-  <title>Web Hypertext Application Technology Working Group File Not Found</title>
-  <link rel="stylesheet" href="/style/tabbed-pages">
-  <link rel="icon" href="https://resources.whatwg.org/logo.svg">
- </head>
- <body>
+<meta charset="utf-8">
+<title>File Not Found — WHATWG</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#3A7908">
+<link rel="icon" href="https://resources.whatwg.org/logo.svg">
+<link rel="stylesheet" href="/style/shared.css">
+<link rel="stylesheet" href="/style/subpages.css">
+
+<header>
+ <hgroup>
   <h1>
-   <span class="the">The</span>
-   <strong class="what">Web Hypertext Application Technology</strong>
-   <span class="wg">Working Group</span>
+   <a href="/">
+    <img id="main-logo" src="https://resources.whatwg.org/logo.svg" alt="">
+    WHATWG
+   </a>
   </h1>
-  <ul class="navigation">
-   <li><a href="/" rel="home">Home</a></li>
-   <li><a href="https://spec.whatwg.org/">Standards</a></li>
-   <li><a href="/faq">FAQ</a></li>
-   <li><a href="/policies">Policies</a></li>
-   <li><a href="https://participate.whatwg.org/">Participate</a></li>
-  </ul>
-
   <h2>File Not Found</h2>
+ </hgroup>
+</header>
 
-  <p>The file you are looking for could not be found.</p>
+<nav class="buttonish-links">
+ <a href="https://spec.whatwg.org/">Standards</a>
+ <a href="/faq">FAQ</a>
+ <a href="/policies">Policies</a>
+ <a href="https://participate.whatwg.org/">Participate</a>
+</nav>
 
-  <p>If you have found a broken link on the WHATWG site itself, please
-  <a href="https://github.com/whatwg/whatwg.org/issues/new">file an issue</a>. If you found a broken
-  link from another site pointing to the WHATWG site, please let that site know of the problem
-  instead. Thanks!</p>
+<p>The file you are looking for could not be found.</p>
 
-  <footer>
-   <p><small>Copyright © 2017 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
-  </footer>
- </body>
-</html>
+<p>If you have found a broken link on the WHATWG site itself, please
+<a href="https://github.com/whatwg/whatwg.org/issues/new">file an issue</a>. If you found a broken
+link from another site pointing to the WHATWG site, please let that site know of the problem
+instead. Thanks!</p>
+
+<footer>
+ <p><small>Copyright © 2018 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
+</footer>

whatwg.org/410
@@ -1,30 +1,34 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="en">
- <head>
-  <title>Web Hypertext Application Technology Working Group Gone</title>
-  <link rel="stylesheet" href="/style/tabbed-pages">
-  <link rel="icon" href="https://resources.whatwg.org/logo.svg">
- </head>
- <body>
+<meta charset="utf-8">
+<title>Gone — WHATWG</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#3A7908">
+<link rel="icon" href="https://resources.whatwg.org/logo.svg">
+<link rel="stylesheet" href="/style/shared.css">
+<link rel="stylesheet" href="/style/subpages.css">
+
+<header>
+ <hgroup>
   <h1>
-   <span class="the">The</span>
-   <strong class="what">Web Hypertext Application Technology</strong>
-   <span class="wg">Working Group</span>
+   <a href="/">
+    <img id="main-logo" src="https://resources.whatwg.org/logo.svg" alt="">
+    WHATWG
+   </a>
   </h1>
-  <ul class="navigation">
-   <li><a href="/" rel="home">Home</a></li>
-   <li><a href="https://spec.whatwg.org/">Standards</a></li>
-   <li><a href="/faq">FAQ</a></li>
-   <li><a href="/policies">Policies</a></li>
-   <li><a href="https://participate.whatwg.org/">Participate</a></li>
-  </ul>
-
   <h2>Gone</h2>
+ </hgroup>
+</header>
 
-  <p>The file you are looking for has been intentionally deleted. Good day!</p>
+<nav class="buttonish-links">
+ <a href="https://spec.whatwg.org/">Standards</a>
+ <a href="/faq">FAQ</a>
+ <a href="/policies">Policies</a>
+ <a href="https://participate.whatwg.org/">Participate</a>
+</nav>
 
-  <footer>
-   <p><small>Copyright © 2017 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
-  </footer>
- </body>
-</html>
+<p>The file you are looking for has been intentionally deleted. Good day!</p>
+
+<footer>
+ <p><small>Copyright © 2018 WHATWG (Apple, Google, Mozilla, Microsoft). This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small></p>
+</footer>

commit f6380452bfdb7ba84008cd4d3bea3561fea9e6e9

Fix logic for computing the title of policies
It turns out lstrip does not do what I thought it did. Fixes the "orkstream Policy" title.

This bug was introduced in 4d34512aeb618c8753e317b220a7937c2161fb11.

convert-policy.py
@@ -87,7 +87,7 @@ def preprocess_markdown(policy_markdown, link_mapping):
 def markdown_title(policy_markdown):
     for line in policy_markdown.split('\n'):
         if line.startswith("# WHATWG "):
-            title = line.lstrip("# WHATWG ")
+            title = line[len("# WHATWG "):]
             policy_markdown = policy_markdown.replace(line, "")
 
             return (title, policy_markdown)