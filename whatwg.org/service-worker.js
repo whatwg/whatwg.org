968af02faaa8eb00673ddbd93411d2895fb772e5
Domenic Denicola <d@domenic.me>
2018-05-09 18:51:19 -0400

    Add service workers to whatwg.org and {spec,idea}.whatwg.org
    Closes #152.

---
+++ whatwg.org/service-worker.js
@@ -0,0 +1,28 @@
+"use strict";
+
+self.cacheKey = "v1";
+self.toCache = [
+  "/",
+  "/shared.css",
+  "/subpages.css",
+  "/img/arrow.svg",
+  "/img/bird-webpage.svg",
+  "/img/checker.svg",
+  "/img/irc.svg",
+  "/img/participate.svg",
+  "/validator/",
+  "/faq",
+  "/irc",
+  "/policies",
+  "/principles",
+  "/style-guide",
+  "/ipr-policy",
+  "/workstream-policy",
+  "/code-of-conduct",
+  "/working-mode",
+  "/sg-agreement",
+  "/sg-policy",
+  "https://resources.whatwg.org/logo.svg"
+];
+
+importScripts("https://resources.whatwg.org/website-service-worker.js");
