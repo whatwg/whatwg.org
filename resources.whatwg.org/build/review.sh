96e562ba01f8a9a92a2a41b61d40756d725f9ce2
Anne van Kesteren <annevk@annevk.nl>
2018-05-11 18:46:41 +0200

    make it clearer why we diff

--- resources.whatwg.org/build/review.sh
+++ resources.whatwg.org/build/review.sh
@@ -14,5 +14,5 @@ cp "$INPUT_FILE" temp
 sed 's/^Group: WHATWG$/&'''$'\n'"Date: $(date +'%Y-%m-%d')/g" temp > "$REVIEW_DRAFT"
 rm temp
 echo "Created Review Draft at $REVIEW_DRAFT"
-echo "Differences with $INPUT_FILE:"
+echo "Please verify that only one line changed relative to $INPUT_FILE:"
 diff -up "$INPUT_FILE" "$REVIEW_DRAFT"
