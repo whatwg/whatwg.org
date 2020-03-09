e2d07ea5a751142beff057c2eb0da5df366e8cd9
Anne van Kesteren <annevk@annevk.nl>
2020-03-09 07:50:29 +0100

    add assert

--- convert-policy.py
+++ convert-policy.py
@@ -82,6 +82,8 @@ def markdown_title(policy_markdown):
 
             return (title, policy_markdown)
 
+    assert False, "Markdown does not contain a line starting with # WHATWG "
+
 
 def main():
     link_mapping_pairs = parse_link_mapping(codecs.open("sg/policy-link-mapping.txt", "r", encoding="utf-8").read())
