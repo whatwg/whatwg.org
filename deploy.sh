whatwg/whatwg.org

commit 988c10c213b511c32942c9fb297b814da763b4ba

Deploy to marquee using GitHub actions (#321)

.github/workflows/deploy.yml
@@ -1,0 +1,25 @@
+name: deploy
+on:
+  push:
+    branches:
+      - master
+jobs:
+  deploy:
+    runs-on: ubuntu-20.04
+    steps:
+    - uses: actions/checkout@v2
+    - uses: actions/setup-python@v2
+      with:
+        python-version: 3.8
+    - uses: actions/setup-node@v1
+      with:
+        node-version: 14
+    - run: pip install commonmark==0.9.1
+    - run: npm install
+    - run: shellcheck deploy.sh
+    - run: shellcheck resources.whatwg.org/build/*.sh
+    - run: bash ./deploy.sh
+      env:
+        SERVER: ${{ secrets.MARQUEE_SERVER }}
+        SERVER_PUBLIC_KEY: ${{ secrets.MARQUEE_PUBLIC_KEY }}
+        SERVER_DEPLOY_KEY: ${{ secrets.MARQUEE_DEPLOY_KEY }}

deploy.sh
@@ -62,23 +62,13 @@ if [[ "$TRAVIS" == "true" ]]; then
 fi
 
 # This ensures that only changes to the master branch get deployed
-if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
     header "Skipping deploy"
 else
     header "Synchronizing content with whatwg.org et al"
-    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-    openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in deploy_key.enc -out deploy_key -d
-    chmod 600 deploy_key
     eval "$(ssh-agent -s)"
-    ssh-add deploy_key
-    echo "$SERVER $SERVER_PUBLIC_KEY" > known_hosts
-    # --verbose isn't used because there are too many files to list them all
-    # without exceeding log size limits:
-    # https://github.com/whatwg/whatwg.org/issues/287
-    rsync --archive --chmod="D755,F644" --compress --delete --stats --log-file="rsync-log.txt" --rsh="ssh -o UserKnownHostsFile=known_hosts" ./whatwg.org ./*.whatwg.org "deploy@$SERVER:/var/www/"
-    scp -o="UserKnownHostsFile=known_hosts" rsync-log.txt "deploy@$SERVER:/var/www/whatwg.org/"
-    echo "Full rsync log at https://whatwg.org/rsync-log.txt"
+    echo "$SERVER_DEPLOY_KEY" | ssh-add -
+    mkdir -p ~/.ssh/ && echo "$SERVER $SERVER_PUBLIC_KEY" > ~/.ssh/known_hosts
+    rsync --verbose --archive --chmod=D755,F644 --compress --delete \
+          ./whatwg.org ./*.whatwg.org "deploy@$SERVER:/var/www/"
 fi

.gitignore
@@ -1,6 +1,3 @@
-deploy_key
-deploy_key.pub
-
 # Keep this list synchronized with https://github.com/whatwg/sg/blob/master/policy-link-mapping.txt
 whatwg.org/ipr-policy
 whatwg.org/workstream-policy

.travis.yml
@@ -1,25 +1,0 @@
-language: python
-python: "3.8"
-
-env:
-  global:
-    - ENCRYPTION_LABEL="aec5871fe877"
-    - SERVER="165.227.248.76"
-    - SERVER_PUBLIC_KEY="ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDt6Igtp73aTOYXuFb8qLtgs80wWF6cNi3/AItpWAMpX3PymUw7stU7Pi+IoBJz21nfgmxaKp3gfSe2DPNt06l8="
-
-install:
-  - pip install commonmark==0.9.1
-  - npm install
-script:
-  - shellcheck deploy.sh
-  - shellcheck resources.whatwg.org/build/*.sh
-  - bash ./deploy.sh
-
-branches:
-  only:
-    - master
-
-notifications:
-  email:
-    on_success: never
-    on_failure: always

commit 64f5fb557110ac599ea3b416dec36a5c757062e0

Run deploy.sh on PRs as well
Also take this chance to simplify the TODO waiting for Bash 4.4. Ubuntu
20.04 appears to have Bash 5.

This regressed in https://github.com/whatwg/whatwg.org/pull/321.

.github/workflows/deploy.yml
@@ -1,8 +1,11 @@
 name: deploy
 on:
+  pull_request:
+    branches:
+    - master
   push:
     branches:
-      - master
+    - master
 jobs:
   deploy:
     runs-on: ubuntu-20.04

deploy.sh
@@ -42,18 +42,13 @@ copy_logo "samsung-internet/samsung-internet.svg" "samsung.svg"
 copy_logo "uc/uc_32x32.png" "uc.png"
 echo ""
 
-# Run the HTML checker only when building on Travis
-if [[ "$TRAVIS" == "true" ]]; then
+# Run the HTML checker only when building on CI
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
     header "Running the HTML checker..."
 
-    # Check the targets given explicitly here, plus most of the extensionless files directly inside
-    # whatwg.org/. This uses https://stackoverflow.com/a/23357277/3191 to get the results of the
-    # find command into an array. TODO: if Travis CI ever gets Bash 4.4, we can use the simpler
-    # version at https://stackoverflow.com/a/54561526/3191.
-    TARGETS=(whatwg.org/news whatwg.org/validator whatwg.org/index.html idea.whatwg.org/index.html spec.whatwg.org/index.html)
-    while IFS=  read -r -d $'\0'; do
-        TARGETS+=("$REPLY")
-    done < <(find whatwg.org -maxdepth 1 -type f ! -name "*.*" ! -name "status-2008-12" -print0)
+    # Check most of the extensionless files in whatwg.org/, plus targets explicitly listed.
+    readarray -d '' TARGETS < <(find whatwg.org -maxdepth 1 -type f ! -name "*.*" ! -name "status-2008-12" -print0)
+    TARGETS+=(whatwg.org/news whatwg.org/validator whatwg.org/index.html idea.whatwg.org/index.html spec.whatwg.org/index.html)
 
     curl --retry 2 --fail --remote-name --location https://github.com/validator/validator/releases/download/linux/vnu.linux.zip
     unzip -qq vnu.linux.zip
@@ -62,13 +57,13 @@ if [[ "$TRAVIS" == "true" ]]; then
 fi
 
 # This ensures that only changes to the master branch get deployed
-if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
-    header "Skipping deploy"
-else
+if [[ "$GITHUB_EVENT_NAME" == "push" && "$GITHUB_REF" == "refs/heads/master" ]]; then
     header "Synchronizing content with whatwg.org et al"
     eval "$(ssh-agent -s)"
     echo "$SERVER_DEPLOY_KEY" | ssh-add -
     mkdir -p ~/.ssh/ && echo "$SERVER $SERVER_PUBLIC_KEY" > ~/.ssh/known_hosts
     rsync --verbose --archive --chmod=D755,F644 --compress --delete \
           ./whatwg.org ./*.whatwg.org "deploy@$SERVER:/var/www/"
+else
+    header "Skipping deploy"
 fi