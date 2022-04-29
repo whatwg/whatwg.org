d75db64dc12c4c5f8ae5f145524f684177fb8823
Domenic Denicola <d@domenic.me>
2022-04-29 15:35:34 -0400

    Add support for <dl class="props">
    This makes declaring struct instances (in the sense of https://infra.spec.whatwg.org/#structs) much easier.

--- resources.whatwg.org/standard-service-worker.js
+++ resources.whatwg.org/standard-service-worker.js
@@ -8,7 +8,7 @@
 
 const standardShortname = location.host.split(".")[0];
 
-const cacheKey = "v7";
+const cacheKey = "v8";
 const toCache = [
   location.origin + "/",
   "https://resources.whatwg.org/spec.css",
