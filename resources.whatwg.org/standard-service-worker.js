f1716c16444cded80c97aa93accbbbc99daeeacd
Domenic Denicola <d@domenic.me>
2024-02-09 17:29:39 +0900

    Update service worker cache version
    We should probably stop hitting ourselves though

--- resources.whatwg.org/standard-service-worker.js
+++ resources.whatwg.org/standard-service-worker.js
@@ -8,7 +8,7 @@
 
 const standardShortname = location.host.split(".")[0];
 
-const cacheKey = "v10";
+const cacheKey = "v11";
 const toCache = [
   location.origin + "/",
   "https://resources.whatwg.org/spec.css",
